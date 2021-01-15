de76e0fb0fbf03268bda3b1aefa1c182eb1f1f55
승윤이 <seung-yun@seung-yun-is-iMac.local>
2021-01-15 20:00:29 +0900

    Initial commit

---
+++ ku-boost-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ku-boost-iOS
+//
+//  Created by 승윤이 on 2021/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ku-boost-iOS/ku_boost_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ku_boost_iOSApp.swift
+//  ku-boost-iOS
+//
+//  Created by 승윤이 on 2021/01/15.
+//
+
+import SwiftUI
+
+@main
+struct ku_boost_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
