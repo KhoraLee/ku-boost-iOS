e88f6fff2a7807a148d8e84591780d246874105a
KhoraLee <khora.lee.0@gmail.com>
2021-02-08 04:22:11 +0900

    Implement fetchRegulatGrade + Add Grade.swift and GradeResponse.swift for json decoding + Implement fetchRegulatGrade() func
    - TODO : save grades to DB

---
+++ ku-boost-iOS/Data/Grade/GradeResponse.swift
@@ -0,0 +1,16 @@
+//
+//  GradeResponse.swift
+//  ku-boost-iOS
+//
+//  Created by 승윤이 on 2021/02/08.
+//
+
+import Foundation
+
+struct GradeResponse : Codable {
+    let grades: [Grade]
+    
+    enum CodingKeys: String, CodingKey {
+        case grades = "DS_GRADEOFSTUDENT"
+    }
+}
--- ku-boost-iOS/Utilities/GradeHandler.swift
+++ ku-boost-iOS/Utilities/GradeHandler.swift
@@ -10,6 +10,9 @@ import Combine
 
 class GradeHandler {
     //학기 : 1 -> 1학기, 2 -> 2학기, 3 -> 여름 계절, 4 -> 겨울 계절
+    
+    static let shared = GradeHandler()
+    
     var Alamo = GradeService.shared.session
     
     func fetchGraduationSimulation(){
@@ -19,8 +22,23 @@ class GradeHandler {
     }
     
     func fetchRegularGrade(year:Int, semester:Int){
-        Alamo.request(GradeRouter.RegularGrade(year: year, semester: semester)).responseJSON { (response) in
-//            debugPrint(response)
+        Alamo.request(GradeRouter.RegularGrade(year: year, semester: semester)).responseJSON { [weak self] (response) in
+            guard let weakSelf = self else { return }
+            switch response.result{
+            case .success(let data):
+                do{
+                    let gradeJson = try JSONDecoder().decode(GradeResponse.self, from: JSONSerialization.data(withJSONObject: data))
+                    for grades in gradeJson.grades{
+                        // TODO : DB에 데이터 삽입
+                        print(grades) // 테스트 로깅
+                    }
+                } catch(let error){
+                    debugPrint(error)
+                }
+            case .failure(let err):
+                debugPrint(err)
+            }
+            return
         }
     }
     
