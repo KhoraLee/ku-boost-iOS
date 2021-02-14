a5e26d3446a4a446ae6495ddd312eb0f9c6a99f4
KhoraLee <khora.lee.0@gmail.com>
2021-02-14 23:53:41 +0900

    feat: Add swipe back gesture

---
+++ ku-boost-iOS/Extensions/View.swift
@@ -0,0 +1,19 @@
+//
+//  View.swift
+//  ku-boost-iOS
+//
+//  Created by 승윤이 on 2021/02/14.
+//
+
+import SwiftUI
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 2
+    }
+}
